3e24b54e94663e1deba29e573b1b7af83f287cb5
Niran <32190023+NiranDx@users.noreply.github.com>
2019-03-21 18:30:36 +0700

    add

---
+++ src/Count.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import { add,minus, store} from './App'
+
+class Count extends Component {
+   render() {
+       return (
+               <div style={{margin: '20px'}}>
+                   Counter: {this.props.number} <br/> 
+                   <button onClick={ this.props.add}>+</button>
+                   <button onClick={ this.props.minus}>-</button>
+               </div>
+       );
+   }
+}
+
+const mapStateToProps = (state) => {
+   return { number: state.number }
+}
+
+const mapDispatchToProps = (dispatch) => {
+   return {
+       add : ()=>dispatch(add()),
+       minus: ()=> dispatch(minus())
+   }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Count);
